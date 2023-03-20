6cfc24f07e6dc29c8839aff89ef3ba28abee45bd
Freddy Tacuri <ftacurip@pichincha.com>
2023-03-19 22:51:39 -0500

    set log id

--- payment-register/src/main/java/org/apache/camel/learn/route/FileRoute.java
+++ payment-register/src/main/java/org/apache/camel/learn/route/FileRoute.java
@@ -7,13 +7,17 @@ public class FileRoute extends RouteBuilder {
 
   @Override
   public void configure() throws Exception {
+
+    //Obtener del servidor SFTP
     from("sftp:localhost:2222/upload?noop=true&username=foo&password=pass&delete=true")
         .to("file:src/backup");
 
+
+    // Validar en la base de datos
     from(
         "stream:file?fileName=src/backup/cardsclients.csv&scanStream=true&scanStreamDelay=1000&retry=true&fileWatcher=true")
         .process(new FacturaProcessor())
-        .to("log:foo");
+        .log("valor ${body}, header: ${header.id-parameter}");
   }
 
 }
